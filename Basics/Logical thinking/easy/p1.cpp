88ddc1b254326fc070ee797cd2db03e542357f69
Smriti Mishra <smriticodedthis@gmail.com>
2026-08-06 00:22:18 +0530

    done

---
+++ Basics/Logical thinking/easy/p1.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+ 
+int main(){
+     ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    long long t;
+    cin >> t;
+ 
+  for(int i = 0 ; i<4 ; i++){
+      for(int j= 0 ; j<4 ; j++){
+         cout<<'*' ;
+          
+      }
+      cout<<"\n";
+  }
+}
