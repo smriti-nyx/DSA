c45a9741e79f81377bebd52a6d39a0be7f72b52f
Smriti Mishra <smriticodedthis@gmail.com>
2026-08-09 22:59:04 +0530

    done

--- Basics/Basic Maths/count all digits of a num.cpp
+++ Basics/Basic Maths/count all digits of a num.cpp
@@ -1,13 +1,30 @@
 class Solution {
 public:
     int countDigit(int n) {
+
         int count = 0 ;
        while(n >0){
-        int lastdigit = n%10 ; 
         n = n/10 ; 
         count++ ; 
+
        }
        return count ;
 
     }
+};
+
+
+
+
+//OPTIMISED VERSION 
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int countDigit(int n) {
+        int count =  (int) (log10(n)  + 1 ); 
+         return count ; 
+    }
+  
 };
