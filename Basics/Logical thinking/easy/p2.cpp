0cba95cbd2f2dccb921cf63c81b45f46739c83f2
Smriti Mishra <smriticodedthis@gmail.com>
2026-08-09 16:39:41 +0530

    done

---
+++ Basics/Logical thinking/easy/p2.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+void print1(int n ){
+    for(int i = 0 ; i < n ; i ++){
+        for(int j  = 0 ; j< i ; j++){
+            cout<<"*";
+        }
+        cout<<"\n" ; 
+    }
+}
+ 
+int main(){
+   print1(5);
+ 
+ 
+  }
