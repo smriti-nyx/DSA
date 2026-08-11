a4447c1a87f7f7a1a5392ce8a904f5ee19806e96
Smriti Mishra <smriticodedthis@gmail.com>
2026-08-12 00:35:24 +0530

    1 to n

--- Basics/Recursion/Print 1 to N using Recursion.cpp
+++ Basics/Recursion/Print 1 to N using Recursion.cpp
@@ -1,12 +1,14 @@
-lass Solution {
+class Solution {
   public:
     void printNumbers(int n) {
         
         
         if(n ==0) return ; //base case
         //if condn not met , do this :
-        printNumbers( n-1);
         cout<<n<<"\n" ;
+        printNumbers( n-1);
+        
 
     }
 };
+
