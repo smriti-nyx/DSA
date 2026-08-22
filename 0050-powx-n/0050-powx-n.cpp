9b23930f8dbd003dd9dcc22712a3ae1624a3f6a5
Smriti Mishra <smriticodedthis@gmail.com>
2026-08-22 17:19:50 +0530

    Time: 0 ms (100%), Space: 8.9 MB (7.71%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans = pow(x , n) ; 
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
