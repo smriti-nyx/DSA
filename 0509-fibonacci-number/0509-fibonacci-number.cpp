12043782fc52ca11a704b68f73424155e6ffcd56
Smriti Mishra <smriticodedthis@gmail.com>
2026-08-27 16:20:23 +0530

    Time: 0 ms (100%), Space: 7.7 MB (65.62%) - LeetHub - Time: O(n), Space: O(1)

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-int fib(int n) {
-        //base case 
+int fib(int n){
     if(n<=1){
         return n ; 
     }
-    //recursive case 
-    int l = fib(n -1) ;
-    int r = fib(n-2) ;
-    return l +r ;
+    int prev2 = 0, prev1 = 1 ;
+      for(int i =2 ; i <= n ; i++){
+        int current = prev1 + prev2 ; 
+        prev2 = prev1 ;
+        prev1 = current ;
+      }
+      return prev1  ; 
 
     }
 };
