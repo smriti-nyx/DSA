ea7ad6fd3a82b6e9f934fd8df285b7edeab2d6a7
Smriti Mishra <smriticodedthis@gmail.com>
2026-08-31 19:47:08 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub - Time: O(n log n), Space: O(n)

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-
+//iy
     bool check(vector<int>& nums) {
-        vector<int>B = nums ; 
-        sort(B.begin() , B.end());
-for(int x = 0 ; x < nums.size() ; x++){
-    bool matches = true ; 
-    for(int i = 0 ; i <nums.size() ; i++){
-//if(nums[i] == B[i]) return true ;
-    if(B[i] != nums[(i+x) % nums.size()]){
-      matches = false ;
-       break ; 
-    } 
+        int n = nums.size() ; 
     
- }
-    if(matches) return true ; 
-}
-    return false ; 
+    //always sort n store this way 
+       vector<int>og = nums ;
+       sort(og.begin() , og.end()) ;
+     //B = nums , A = og 
+
+     //to find the index of the minm elemnt 
+
+     int minidx = min_element(nums.begin() , nums.end()) - nums.begin(); //addressof minvalue - address of idx =0 
+
+
+       for(int i =0 ; i <n;i++){
+        if(nums[i] != og[(i+(nums.size() - minidx))%nums.size()]){
+            return false ; 
+        }
+       }
+      return true ; 
     }
 };
 
 // Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
+// Pro features: https://bit.ly/leethubpro | Free version: https://b
