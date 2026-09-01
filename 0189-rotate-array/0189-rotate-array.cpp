4998c458cddd207a08d61f28db024df32f457f1d
Smriti Mishra <smriticodedthis@gmail.com>
2026-09-01 20:58:36 +0530

    Time: 7 ms (11.97%), Space: 263.5 MB (11.22%) - LeetHub - Time: O(n), Space: O(1)

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -4,23 +4,19 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size(); 
         k=k%n;
+       
 
-        // 1,2,3,4 moved in temp 
-        vector<int>temp(n-k) ;
-        for(int i = 0; i <n-k; i ++){
-            temp[i] =nums[i] ; //temp[0] = nums[4] ,1-5, 2-6 ,k =3 
-        }
-         
-         //5,6,7 moved frontow
-        for(int i = n-k ; i <n ; i++){
-            nums[i - (n-k)] = nums[i];
-           // 0-4, 1-5 , 2-6 
-        }
-        //adding 1, 2,3,4 back to nums at 3,4,5,6
-        for(int i = k ; i <n; i++){
-           nums[i] =temp[i-k];
-           //6-3 , 4 n-i , 6-3 , 6-2
-        }
+       //reversing the entire array
+       reverse(nums.begin() , nums.end()) ;
+
+
+       // 0-3 
+       reverse(nums.begin()+0, nums.begin() +k ) ;
+
+       //reversing 4-6 
+       reverse(nums.begin() + k , nums.begin()+(n-1)+1);
+                                   //or nums.end()
+       ; 
         
     }
 };
