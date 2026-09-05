691c6220160eb49978736f14f6831f7a57b90985
Smriti Mishra <smriticodedthis@gmail.com>
2026-09-05 15:55:10 +0530

    Time: 0 ms (100%), Space: 31.5 MB (7.15%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
-//iterative version
+//recursive version
+int binarysearch(int high , int low , int target , vector<int>& nums , int n){
+    //base case
+    if(low>high){
+        return -1 ;
+    }
+    int mid = (low + high)/2 ; 
+
+    //recursive case
+    if(target == nums[mid]){
+        return mid ; 
+    }
+    else if(target > nums[mid]){
+       return binarysearch(n-1 , mid+1,target , nums ,n);
+    }
+    else{
+        return binarysearch(mid-1 ,low , target , nums, n );
+    }
+
+
+}
     int search(vector<int>& nums, int target) {
         int n= nums.size();
-        int low = 0;
-        int high = n-1 ; 
+       // int mid = (low+high)/2 ;
         
-        while(low<= high){
-            int mid = (low+high)/2 ;
-            if(target ==nums[mid]) return mid ; 
-            else if(target>nums[mid]) low = mid+1;
-            else high = mid -1;
-        }
-        return -1 ; 
+        
+       return  binarysearch(n-1 , 0 ,target , nums ,n);
+        
+        
     }
 };
 
