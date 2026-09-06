7a3e2b18d5e9af6d8b74f5acda87591c8e8ff3ae
Smriti Mishra <smriticodedthis@gmail.com>
2026-09-06 16:10:28 +0530

    not acceptable since time complexity is o(n)

---
+++ Binary Search/BS1/first and last occurrence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        //brute approach 
+        int n = nums.size();
+        int first = -1 ; 
+        int last = -1 ; 
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i] == target){
+                if(first == -1){
+                    first = i ; 
+                    last = i ; 
+                }
+                last = i ; 
+            }
+            
+        }
+        return { first , last} ; 
+    }
+};
