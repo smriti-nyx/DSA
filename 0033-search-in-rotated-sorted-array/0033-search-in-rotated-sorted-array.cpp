85672ec0b4283dba03fb15c9615f71475561a9b5
Smriti Mishra <smriticodedthis@gmail.com>
2026-09-08 00:24:35 +0530

    Time: 0 ms (100%), Space: 15.3 MB (37.02%) - LeetHub - Time: O(log n), Space: O(1)

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,13 +1,35 @@
 class Solution {
 public:
+//use binary search 
+//find mid 
+//check if right is sorted , check if target lies in the right range .
+//do the same for left .
+//for ex [ 4,5,1,1,2,3,4] is not sorted  whereas [4,5,6,7,0,1,2] is sorted .
     int search(vector<int>& nums, int target) {
         int n = nums.size();
-        for(int i = 0 ; i <n ; i++){
-            if(nums[i] == target){
-                return i ; 
+         int low = 0 ,  high = n-1 ; 
+
+         while(low<= high){
+            int mid = (low +high)/2 ; 
+            if(nums[mid] == target) return mid ; 
+
+            if(nums[low]<= nums[mid]){
+                 if(target>= nums[low] && target<= nums[mid]){
+                    high = mid -1 ;
+                 }else{
+                    low = mid+1 ; 
+                 }
+                 }else{
+                    if(nums[mid]<=target&& nums[high]>= target){
+                        low = mid+1 ; 
+                    }else{
+                  high = mid-1 ;
+                    }
+                    
+                 }
             }
-        }
-        return -1 ; 
+         
+        return -1 ;
     }
 };
 
