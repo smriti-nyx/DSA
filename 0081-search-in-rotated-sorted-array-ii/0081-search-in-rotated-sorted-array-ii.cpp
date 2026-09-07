5f60134076bdaa2653760fdc97710aa1e68145f1
Smriti Mishra <smriticodedthis@gmail.com>
2026-09-08 00:18:41 +0530

    Time: 0 ms (100%), Space: 18 MB (80.95%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int high = n-1 ;
+         int low = 0 ; 
+
+        while(low<=high){
+            int mid = (low+high)/2 ; 
+
+            if(nums[mid] == target) return true ; 
+              
+              if(nums[low] == nums[mid] && nums[mid] == nums[high]){
+                high-- , low++ ; 
+                continue ; 
+              }
+            //checking if left half is sorted (ascending)
+            if(nums[low]<= nums[mid]){
+                if(target>=nums[low]&&target<=nums[mid]){
+                    high = mid -1 ; 
+                }else{
+                    low = mid+1 ; 
+                }
+            }
+            //for the right half 
+            else{
+                if(target<=nums[high]&& target>=nums[mid]){
+                    low = mid+1 ; 
+                }else{
+                    high = mid -1 ; 
+                }
+            }
+
+        }
+        return false ;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
