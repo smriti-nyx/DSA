517bd883bb5e99522bcf4dd291bfc18696f62e6e
Smriti Mishra <smriticodedthis@gmail.com>
2026-09-09 21:37:10 +0530

    Time: 0 ms (100%), Space: 63.4 MB (98.04%) - LeetHub - Time: O(log n), Space: O(1)

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& nums) {
+        int n = nums.size() ; 
+        if(n==1) return 0 ;
+        if(nums[0]>nums[1]) return nums[0];
+        if(nums[n-1]>nums[n-2]) return nums[n-1];
+        int high = n-2 ; 
+        int low = 1 ; 
+        while(low<=high){
+            int mid = (low+high)/2 ; 
+            if(nums[mid]>nums[mid+1]&&nums[mid]>nums[mid-1]){
+                return mid ; 
+            }
+            if(nums[mid]<nums[mid+1]){
+                 low = mid+1 ; 
+            }else{
+                high = mid -1 ; 
+            }
+        }
+                return 0 ; 
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
