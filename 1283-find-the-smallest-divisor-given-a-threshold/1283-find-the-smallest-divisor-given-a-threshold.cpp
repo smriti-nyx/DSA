b19c8d4320cb5a17a911d92667dc2177f8114b31
Smriti Mishra <smriticodedthis@gmail.com>
2026-09-11 22:51:54 +0530

    Time: 6 ms (68.53%), Space: 26.2 MB (14.82%) - LeetHub - Time: O(n * log(max(nums))), Space: O(1)

---
+++ 1283-find-the-smallest-divisor-given-a-threshold/1283-find-the-smallest-divisor-given-a-threshold.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+int findsum(vector<int>& nums ,int divisor ){
+    int sum = 0;
+    for(int i = 0 ; i <nums.size() ; i++){
+     sum += (nums[i] + divisor -1)/divisor ;
+    }
+    return sum;
+}
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int n = nums.size() ; 
+        int low = 1 ; 
+        int high = *max_element(nums.begin(),nums.end()) ; 
+        int ans = INT_MIN ; 
+        while(low<=high){
+            int mid = (low+high)/2 ; 
+             int mythreshold = findsum(nums , mid);
+             if(mythreshold>threshold){
+                 low = mid+1;
+              //  high = mid-1 ; 
+             }
+             else{
+                ans = mid ;
+                high = mid-1 ; 
+             }
+        }
+        return ans ; 
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
