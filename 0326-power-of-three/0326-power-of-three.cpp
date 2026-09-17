a8489c5ba5b5b6e18eaf5405fffe2c5ecc29f5ad
Smriti Mishra <smriticodedthis@gmail.com>
2026-09-17 11:56:55 +0530

    Time: 2 ms (66%), Space: 8.8 MB (74.53%) - LeetHub - Time: O(log n), Space: O(1)

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,23 +1,12 @@
 class Solution {
 public:
-  bool power(int n ){
-    
-    if(n ==1){
-    return true ;
-}   
-    //recursive case 
-    while(n%3==0){
-        n= n/3;
-    }
-
-    return n==1 ;  //returns true if n comes out ot be 1 in th ene d
-  }
     bool isPowerOfThree(int n) {
         if(n<=0) return false ; 
-        if(n==1) return true ; 
-        if(n%3!= 0) return false ; 
-        
-        return power(n);
+        //if(n%3!= 0) return false ; 
+        while(n%3==0){
+            n/=3 ; 
+        }
+        return n==1 ; 
     }
 };
 
