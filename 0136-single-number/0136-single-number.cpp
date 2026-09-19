df6b887e9ea48d7caa0dce3fcb9cae2596d36047
Smriti Mishra <smriticodedthis@gmail.com>
2026-09-19 22:26:54 +0530

    Time: 0 ms (100%), Space: 20.8 MB (24.1%) - LeetHub - Time: O(n), Space: O(1)

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-       // we will perform xor bitwise op w 0 , since xor of a no. with 0 gives itself and the xor of number w iteself gives 0. hence if the final answer isnt zero , there exists a number which appears only once. return that number.
+       // we will perform xor bitwise op w 0 , since xor of a no. with 0 gives  the num itself and the xor of number w iteself gives 0. hence if the final answer isnt zero , there exists a number which appears only once. return that number.
        int ans = 0 ; 
        for(auto x: nums ){
         ans = ans^x ; 
@@ -10,6 +10,8 @@ public:
        
     }
 };
+
+//Additionally, XOR is both commutative and associative.This means the order in which numbers are XORed does not matter. so if the ans is 0 , there are all duplicates , else the num is hte output . 
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
