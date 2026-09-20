26db5fa5f3f2814e09de15f095b9f0956dac588a
Smriti Mishra <smriticodedthis@gmail.com>
2026-09-20 22:15:12 +0530

    Time: 0 ms (100%), Space: 9.7 MB (96.07%) - LeetHub - Time: O(2^n * n), Space: O(2^n * n)

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
-void subsets(int idx ,vector<int>& nums , vector<int>& ds , vector<vector<int>>& ans ){
-    //base case 
-     if(idx==nums.size()){ // not > used coz noooo.
-      //  ds.push_back(nums[idx]); because ds already contains all the lemnts of nums 
-        ans.push_back(ds);
-        return ; 
-    }
-    
-    //recursive case 
-    ds.push_back(nums[idx]) ; 
-    subsets(idx+1 , nums , ds , ans); 
-
-    ds.pop_back(); 
-    subsets(idx+1 , nums, ds , ans);
-    
-}
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int>ds ; 
-        vector<vector<int>>ans ; 
-            subsets(0 , nums , ds, ans);
-        return ans ; 
+        int n = nums.size(); // ex = 3 
+        int subsets = 1<<nums.size();// ex = 8 . no.of subssets that will be formed 
+        // 2^nbinary combinations 
+        vector<vector<int>>ans; 
+        for(int i = 0 ; i <subsets ; i++){ // 000 , 001 , 010 ,.....
+            vector<int>ds;
+            for(int j = 0 ; j <n ; j++){ //ex - 0 0 1
+                if((i&(1<<j))!=0){
+                    ds.push_back(nums[j]) ; 
+                }
+            } 
+                ans.push_back(ds); 
+        }
+            return ans ; 
     }
 };
 
