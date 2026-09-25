a398e5ec90d86134256f33d3af110fb94e7304ba
Smriti Mishra <smriticodedthis@gmail.com>
2026-09-25 21:35:11 +0530

    Time: 0 ms (100%), Space: 29.6 MB (56.89%) - LeetHub - Time: O(n), Space: O(n)

--- 1720-decode-xored-array/1720-decode-xored-array.cpp
+++ 1720-decode-xored-array/1720-decode-xored-array.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     vector<int> decode(vector<int>& encoded, int first) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         vector<int>arr(encoded.size()+1);
         arr[0] = first ; 
 
         
         for(int i = 0 ; i <encoded.size() ; i++){
-            arr[i+1] = ((encoded[i]^arr[i])) ; 
+            arr[i+1] = arr[i]^encoded[i];
             
         }
         return arr ; 
